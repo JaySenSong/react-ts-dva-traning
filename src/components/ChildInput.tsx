962d65095ec80b404070bdaa2387d24b74956bea
JaySenSong <155499008+JaySenSong@users.noreply.github.com>
2025-06-24 09:44:26 +0800

    Update ChildInput.tsx

--- src/components/ChildInput.tsx
+++ src/components/ChildInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 const ChildInput = forwardRef((_, ref) => {
     const inputRef = useRef<HTMLInputElement>(null);
